910a18be788a27fd9268de9ad316db32e05a1292
Pranjal <lppranjal@gmail.com>
2025-02-09 23:52:47 +0530

    removed everything from angular project for fresh start

--- JK-client/src/app/app.module.ts
+++ JK-client/src/app/app.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';  // ✅ Import NgModule
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { TokenInterceptor } from './interceptors/token.interceptor';
 import { AppRoutingModule } from './app-routing.module'; // Import Routing Module
-import { LoginComponent } from './components/login/login.component';
-import { ProtectedComponent } from './components/protected/protected.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,  // ✅ Added
-    ProtectedComponent // ✅ Added
   ],
   imports: [
     BrowserModule,
@@ -19,7 +14,7 @@ import { ProtectedComponent } from './components/protected/protected.component';
     AppRoutingModule
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
+    
   ],
   bootstrap: [AppComponent]
 })
